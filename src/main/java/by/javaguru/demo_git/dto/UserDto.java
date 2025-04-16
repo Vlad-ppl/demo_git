252e62fc09f44de098e660d9c113467985bb1646
Vlad-ppl <melnik.vlad1990@gmail.com>
2025-04-16 18:09:36 +0200

    UserServise + UserDto

---
+++ src/main/java/by/javaguru/demo_git/dto/UserDto.java
@@ -0,0 +1,16 @@
+package by.javaguru.demo_git.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private int id;
+    private String name;
+}
---
+++ src/main/java/by/javaguru/demo_git/service/UserService.java
@@ -0,0 +1,33 @@
+package by.javaguru.demo_git.service;
+
+import by.javaguru.demo_git.dto.UserDto;
+import by.javaguru.demo_git.entity.UserEntity;
+import by.javaguru.demo_git.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Optional<UserEntity> findById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public List<UserEntity> findAll() {
+        return userRepository.findAll();
+    }
+
+    public UserEntity save(@NotNull UserDto userDto) {
+        UserEntity userEntity = UserEntity.builder()
+                .name(userDto.getName())
+                .build();
+        return userRepository.save(userEntity);
+    }
+}
