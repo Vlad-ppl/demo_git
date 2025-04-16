e30f30c1ccbb78a26c8b583380c8b4f144d4a59e
Vlad-ppl <melnik.vlad1990@gmail.com>
2025-04-16 16:59:04 +0200

    Add new class UserEntity

---
+++ src/test/java/by/javaguru/demo_git/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package by.javaguru.demo_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
